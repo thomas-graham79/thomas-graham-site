a71f7732416be0e315ecd655f0ba08adfbf1d958
thomas-graham79 <107213603+thomas-graham79@users.noreply.github.com>
2025-05-29 12:27:02 -0400

    Update portfolio.tsx

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent } from "../components/ui/card";
 
 export default function Portfolio() {
   return (
